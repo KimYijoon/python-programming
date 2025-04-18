22239861af600f1969e8a901aefb2b3f19e5b618
KimYijoon <cillyda42@gmail.com>
2025-04-19 00:55:07 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,16 @@
+def display_multiplication_table() :
+    for i in range(1, 10) :
+        for n in range(2, 6) :
+            print(f'{n} x {i} = {n * i:2d}', end='\t')
+            
+        print()
+        
+    print()  
+
+    for i in range(1, 10) :
+        for n in range(6, 10) :
+            print(f'{n} x {i} = {n * i:2d}', end='\t')
+
+        print()
+
+display_multiplication_table()
